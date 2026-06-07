dd9eea9323f9aaeb8acf9807eba95571111fd022
Nas Villanueva <nasvillanuevait@gmail.com>
2026-06-07 21:47:33 +0900

    Bring back clock landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
+  import dayjs from "dayjs";
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
+  import { writable } from "svelte/store";
+  import { onMount } from "svelte";
 
   let { children } = $props();
+
+  const darkMode = writable(false);
+
+  onMount(() => {
+    darkMode.subscribe((newDarkMode) => {
+      document?.documentElement.classList.toggle("dark", newDarkMode);
+    });
+
+    const toggleDarkMode = () => {
+      const currentHour = dayjs().hour();
+
+      darkMode.set(currentHour >= 18 || currentHour <= 9);
+    };
+
+    const checkDarkModeInterval = setInterval(toggleDarkMode, 1000 * 60);
+
+    toggleDarkMode();
+
+    return () => {
+      clearInterval(checkDarkModeInterval);
+    };
+  });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
