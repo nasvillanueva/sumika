657164399f8a7cda98ef1d3d8c8c6701924f5501
Nas Villanueva <nasvillanuevait@gmail.com>
2026-06-07 19:14:52 +0900

    Initialize SvelteKit project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
+
+export default defineConfig({
+  server: {
+    allowedHosts: ["nexus.local"],
+  },
+  plugins: [tailwindcss(), sveltekit()],
+  test: {
+    globals: true,
+    expect: { requireAssertions: true },
+    projects: [
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "server",
+          environment: "node",
+          include: ["src/**/*.{test,spec}.{js,ts}"],
+          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+        },
+      },
+    ],
+  },
+});
