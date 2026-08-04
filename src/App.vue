0987dff43b8019a7676e1272757bc6cddec650de
Xyza Rivera <xyzarivera@gmail.com>
2026-08-04 22:05:03 +0900

    setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="py-24 sm:py-32">
+    <h1
+      class="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl"
+    >
+      You did it!
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
